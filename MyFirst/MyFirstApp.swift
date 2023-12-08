bbef8427cfa4a0b1ee00688adf75605a68aa709e
DJmatsunaga <naritari0616@icloud.com>
2023-12-09 01:49:10 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by 成田陸人 on 2023/12/09.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
